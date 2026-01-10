5ad46f0051e66237e86ae2bbb143bec83b573a86
Nehuen Lian <nehuenn.lian@gmail.com>
2026-01-09 21:17:41 -0300

    change fake function name

--- tests/unit/test_xml_builder.py
+++ tests/unit/test_xml_builder.py
@@ -3,13 +3,13 @@ from service.xml_management.xml_builder import build_login_ticket_request, parse
 
 def test_build_login_ticket_request():
 
-    def fake_time():
+    def fake_time_provider():
         return (
             1767764408,
             "2026-01-07T05:40:08Z",
             "2026-01-07T05:50:08Z",
         )
 
-    root = build_login_ticket_request(fake_time)
+    root = build_login_ticket_request(fake_time_provider)
 
     assert root.find("header/uniqueId").text == "1767764408"
