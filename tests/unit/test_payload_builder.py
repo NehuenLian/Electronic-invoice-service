80afe4accf1b322e4bee9b4850423662db13260d
Nehuen Lian <nehuenn.lian@gmail.com>
2026-02-05 16:40:54 -0300

    remove unit test for old build_auth function

--- tests/unit/test_payload_builder.py
+++ tests/unit/test_payload_builder.py
@@ -11,12 +11,3 @@ def test_add_auth_to_payload():
 
     assert fake_sale_data_with_auth['Auth']['Token'] == fake_token
     assert fake_sale_data_with_auth['Auth']['Sign'] == fake_sign
-
-
-def test_build_auth():
-
-    auth = build_auth("fake_token", "fake_sign", 30740253022)
-
-    assert auth["Token"] == "fake_token"
-    assert auth["Sign"] == "fake_sign"
-    assert auth["Cuit"] == 30740253022
