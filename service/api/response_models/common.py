9ddc0983e80ce23a59005ad979ab3af0ffb2a4e4
Nehuen Lian <nehuenn.lian@gmail.com>
2026-02-15 23:20:56 -0300

    add common models and enable populate_by_name

--- service/api/response_models/common.py
+++ service/api/response_models/common.py
@@ -1,5 +1,5 @@
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class Obs(BaseModel):
@@ -7,6 +7,9 @@ class Obs(BaseModel):
     Msg: str | None = None
 
 class Observaciones(BaseModel):
+
+    model_config = ConfigDict(populate_by_name=True)
+
     obs: list[Obs] | None = Field(None, alias="Obs")
 
 class Evt(BaseModel):
@@ -23,6 +26,33 @@ class Err(BaseModel):
 class Errors(BaseModel):
     Err: list[Err]
 
+class FECAEASinMov(BaseModel):
+    CAEA: str | None = None
+    FchProceso: str | None = None
+    PtoVta: int
+
+class FECAEAGet(BaseModel):
+
+    model_config = ConfigDict(populate_by_name=True)
+
+    CAEA: str | None = None
+    Periodo: int
+    Orden: int        
+    FchVigDesde: str | None = None
+    FchVigHasta: str | None = None
+    FchTopeInf: str | None = None
+    FchProceso:  str | None = None
+    
+    observaciones: Observaciones | None = Field(None, alias="Observaciones")
+
+class FECAEAGetResponse(BaseModel):
+
+    model_config = ConfigDict(populate_by_name=True)
+
+    fecaea_get: FECAEAGet | None = Field(None, alias="FECAEAGet")
+
+    events: Events | None = Field(None, alias="Events")
+    errors: Errors | None = Field(None, alias="Errors")
 
 ######### Infrastructure #########
 class ErrorDetails(BaseModel):
